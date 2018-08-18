aefafc05a1080792e353a6b6c5d822428df62b7d
littleluckly <707798472@qq.com>
2018-08-18 17:33:48 +0800

    首页

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,66 @@
 <template>
 	<div id="app">
-		<div id="nav">
-			<router-link to="/">Home</router-link> |
-			<router-link to="/about">About</router-link>
-		</div>
-		<router-view/>
+		<el-container>
+			<el-header>
+				<Header/>
+			</el-header>
+			<el-main>
+				<router-link to="/">Home</router-link> |
+				<router-link to="/about">About</router-link>
+				<router-view/>
+			</el-main>
+			<el-footer>Footer</el-footer>
+		</el-container>
 	</div>
 </template>
-<script>
 
+<script>
+	import Header from './views/Header.vue'
+	export default{
+		components:{
+			Header
+		},
+	}
 </script>
-<style lang="less">
+
+<style scoped>
 	#app {
-		font-family: "Avenir", Helvetica, Arial, sans-serif;
-		-webkit-font-smoothing: antialiased;
-		-moz-osx-font-smoothing: grayscale;
-		text-align: center;
-		color: #2c3e50;
+		width: 1300px;
+		margin: 0 auto;
 	}
 
-	#nav {
-		padding: 30px;
-		a {
-			font-weight: bold;
-			color: #2c3e50;
-			&.router-link-exact-active {
-				color: #42b983;
-			}
-		}
+	.el-header,
+	.el-footer {
+		/* background-color: #B3C0D1; */
+		color: #333;
+		text-align: center;
+		line-height: 60px;
+	}
+
+	.el-aside {
+		background-color: #D3DCE6;
+		color: #333;
+		text-align: center;
+		line-height: 200px;
+	}
+
+	.el-main {
+		background-color: #E9EEF3;
+		color: #333;
+		text-align: center;
+		/* line-height: 160px; */
+	}
+
+	body>.el-container {
+		margin-bottom: 40px;
+	}
+
+	.el-container:nth-child(5) .el-aside,
+	.el-container:nth-child(6) .el-aside {
+		line-height: 260px;
+	}
+
+	.el-container:nth-child(7) .el-aside {
+		line-height: 320px;
 	}
 </style>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,31 +1,31 @@
 
 <template>
-  <div class="about">
-    <h1>{{title}}</h1>
-    <el-button>默认按钮</el-button>
-    <el-button type="primary" @click.native="changeTitle({newTitle:'我是新的顶顶顶顶顶'})">改变标题</el-button>
-    <el-button type="success">成功按钮</el-button>
-    <el-button type="info">信息按钮</el-button>
-    <el-button type="warning">警告按钮</el-button>
-    <el-button type="danger">危险按钮</el-button>
-  </div>
+	<div class="about">
+		<h1>{{title}}</h1>
+		<el-button>默认按钮</el-button>
+		<el-button type="primary" @click.native="changeTitle({newTitle:'我是新的顶顶顶顶顶'})">改变标题</el-button>
+		<el-button type="success">成功按钮</el-button>
+		<el-button type="info">信息按钮</el-button>
+		<el-button type="warning">警告按钮</el-button>
+		<el-button type="danger">危险按钮</el-button>
+	</div>
 </template>
 
 <script>
-	import Vue from "vue"
 	import {
-		Button
-	} from "element-ui";
-	Vue.use(Button);
-	import { createNamespacedHelpers } from 'vuex';
-	const { mapState,mapActions } = createNamespacedHelpers('aboutStore');
+		createNamespacedHelpers
+	} from 'vuex';
+	const {
+		mapState,
+		mapActions
+	} = createNamespacedHelpers('aboutStore');
 
-export default {
-	computed:{
-		...mapState(['title'])
-	},
-	methods:{
-		...mapActions(['changeTitle'])
+	export default {
+		computed: {
+			...mapState(['title'])
+		},
+		methods: {
+			...mapActions(['changeTitle'])
+		}
 	}
-}
 </script>
